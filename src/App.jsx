5742cfb1fa2e7bdf6ad7988682eaf24485e0b846
chango <kimtothechang@gmail.com>
2022-06-08 15:23:16 +0900

    update: meta 태그 설정

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { routes } from './routes';
 
+import { Helmet } from 'react-helmet-async';
+
 import ScrollToTop from './utils/ScrollToTop';
 import './reset.css';
 
 function App() {
   return (
     <BrowserRouter>
+      <Helmet>
+        <title>오픈 마켓</title>
+        <link rel="icon" type="image/png" sizes="16x16" href={`${process.env.PUBLIC_URL}/favicon-16x16.png`} />
+      </Helmet>
       <ScrollToTop />
       <Routes>
         {routes.map((page) => (
--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import App from './App';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <RecoilRoot>
-      <App className="app" />
-    </RecoilRoot>
+    <HelmetProvider>
+      <RecoilRoot>
+        <App className="app" />
+      </RecoilRoot>
+    </HelmetProvider>
   </React.StrictMode>
 );
